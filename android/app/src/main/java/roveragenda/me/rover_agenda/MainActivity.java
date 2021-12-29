6bc3ee17f1224811ee2ecf2033485f8c89fcf656
Karolina Kotlarz <kotlarzkarolina16@gmail.com>
2021-12-29 12:54:33 -0500

    Base Project

---
+++ android/app/src/main/java/roveragenda/me/rover_agenda/MainActivity.java
@@ -0,0 +1,6 @@
+package roveragenda.me.rover_agenda;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/flyout_menu.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              '',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.red,
+                image: DecorationImage(
+                    fit: BoxFit.fitWidth,
+                    image: AssetImage('images/rover_agenda_icon.png'))),
+          ),
+          ListTile(
+            leading: Icon(Icons.book_outlined),
+            title: Text('Schedule'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.fastfood_outlined),
+            title: Text('Lunch Menu'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.people_alt_outlined),
+            title: Text('Teachers'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.widgets_outlined),
+            title: Text('Extracurriculars'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.calendar_today_sharp),
+            title: Text('School Calendar'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings_outlined),
+            title: Text('Settings'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      ),
+    );
+  }
+}
