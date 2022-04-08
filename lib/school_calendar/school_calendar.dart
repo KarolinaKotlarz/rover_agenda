b8d15da511c6149be027533054d8e57c8cc02af0
Karolina Kotlarz <kotlarzkarolina16@gmail.com>
2022-04-08 18:17:10 -0400

    Instabug integrated completely
    Added bug report button on flyout menu, rearranged tiles on settings page

--- lib/school_calendar/school_calendar.dart
+++ lib/school_calendar/school_calendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instabug_flutter/Instabug.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import '../flyout_menu.dart';
 import '../globals.dart' as globals;
@@ -6,6 +7,11 @@ import '../globals.dart' as globals;
 class SchoolCalendar extends StatelessWidget {
   const SchoolCalendar({Key? key}) : super(key: key);
   static String routeName = "/extracurriculars";
+
+  @override
+  void initState() {
+    Instabug.start('76ed198e8e1d4438e3ff5b8b152d6e60', [InvocationEvent.shake]);
+  }
 
   @override
   Widget build(BuildContext context) {
