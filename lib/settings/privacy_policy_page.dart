53e9e337f8c9d32985f7309b2a38e292096ae5e1
Emma <emmali.usa@gmail.com>
2022-04-08 19:24:54 -0400

    Implemented .txt reading code

--- lib/settings/privacy_policy_page.dart
+++ lib/settings/privacy_policy_page.dart
@@ -1,22 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
 
 import '../flyout_menu.dart';
 import '../globals.dart' as globals;
 
-
-class PrivacyPolicy extends StatelessWidget {
+class PrivacyPolicy extends StatefulWidget {
   const PrivacyPolicy({Key? key}) : super(key: key);
+
+  @override
+  _PrivacyPolicyState createState() => _PrivacyPolicyState();
+}
+
+class _PrivacyPolicyState extends State<PrivacyPolicy> {
+  Future<String> getFileData(String path) async {
+    final String data = await rootBundle.loadString(path);
+    return data;
+  }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Privacy Policy'),
+        title: Text('Privacy Policy'),
       ),
-      body: Center(
-        child: Text("Our Privacy Policy"),
-      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: FutureBuilder<String>(
+            future: getFileData('assets/placeholder.txt'),
+            builder:
+            (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.hasData) {
+                final theText = snapshot.data;
+                return Text(theText!);
+              } else {
+                return Text('loading...');
+              }
+            },
+          )
+        ),
+      )
     );
   }
 }
